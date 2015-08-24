ce3c352492d1b0195ebfb7582bbc3f9156b8c0ac
ksami <ksami.ihide@gmail.com>
2015-08-24 18:11:08 +0800

    fix shell not found, add check for vundle

--- setup.sh
+++ setup.sh
@@ -27,7 +27,6 @@ fi
 BASE_DIR=$(cd $(dirname $0);pwd)
 
 for FILE in $SYMLINK_LIST; do
-
     # Backup existing files
     if [ -e ~/$FILE ]; then
         echo "Backing up ~/$FILE to ~/$FILE.old"
@@ -42,14 +41,15 @@ done
 
 
 # Set shell to zsh
-if cat /etc/shells | grep -q "zsh"; then
-    PATH_TO_ZSH=$(which zsh)
-    if [ $SHELL != $PATH_TO_ZSH ]; then
-        echo "Changing shell to zsh"
-        chsh -s $PATH_TO_ZSH
+if grep -q "zsh" /etc/shells; then
+    TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+        echo "Changing default shell to zsh"
+        chsh -s $(grep /zsh$ /etc/shells | tail -1)
     else
         echo "Already using zsh!"
     fi
+    unset TEST_CURRENT_SHELL
 else
     echo "zsh not found, please install zsh then change shell"
 fi
@@ -58,9 +58,13 @@ fi
 
 # Install Vundle
 if command -v vim &>/dev/null; then
-    echo "Installing Vundle"
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
+    if [ ! -e "~/.vim/bundle/Vundle.vim" ]; then
+        echo "Installing Vundle"
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        vim +PluginInstall +qall
+    else
+        echo "Vundle already installed"
+    fi
 else
     echo "Vim not found, please install vim then install Vundle"
 fi
@@ -76,31 +80,17 @@ if [ ! -n "$ZSH" ]; then
 fi
 
 if [ -d "$ZSH" ]; then
-  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove $ZSH if you want to install"
+  echo "You already have Oh My Zsh installed. You'll need to remove $ZSH if you want to install"
   exit
 fi
 
-echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+echo "Cloning Oh My Zsh..."
 hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
   echo "git not installed"
   exit
 }
 
-TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
-    chsh -s $(grep /zsh$ /etc/shells | tail -1)
-fi
-unset TEST_CURRENT_SHELL
-
-echo "\033[0;32m"'         __                                     __   '"\033[0m"
-echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-echo "\033[0;32m"'                        /____/                       ....is now installed!'"\033[0m"
-echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.\033[0m"
-echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/ohmyzsh.\033[0m"
-echo "\n\n \033[0;32mp.p.s. Get stickers and t-shirts at http://shop.planetargon.com.\033[0m"
 env zsh
+
+# Execute commands from file in current shell
 . ~/.zshrc
