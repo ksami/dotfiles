761c8174cb330edf2194a9c23aab7aa56578be2c
ksami <ksami.ihide@gmail.com>
2016-06-01 18:57:10 +0800

    add more colors and times to workclock

--- today-scripts/workclock.sh
+++ today-scripts/workclock.sh
@@ -1,5 +1,25 @@
-if [ $(date +'%k') -ge 9 -a $(date +'%k') -le 17 ]; then
-    echo "\e[32m$(date +'%l:%M %p')";
+FG_DEFAULT="39"
+FG="3"
+BG="4"
+DEFAULT="9"
+BLACK="0"
+RED="1"
+GREEN="2"
+YELLOW="3"
+BLUE="4"
+MAGENTA="5"
+CYAN="6"
+LGRAY="7"
+
+TIME_24H=$(date +'%k')
+TIME_FULL=$(date +'%l:%M %p')
+
+if [ $TIME_24H -ge 9 -a $TIME_24H -lt 12 ]; then
+    echo "\e["$FG$GREEN";"$BG$DEFAULT"m"$TIME_FULL
+elif [ $TIME_24H -ge 12 -a $TIME_24H -lt 14 ]; then
+    echo "\e["$FG$GREEN";"$BG$BLUE"m"$TIME_FULL
+elif [ $TIME_24H -ge 14 -a $TIME_24H -lt 17 ]; then
+    echo "\e["$FG$GREEN";"$BG$LGRAY"m"$TIME_FULL
 else
-    echo "\e[31m$(date +'%l:%M %p')";
+    echo "\e["$FG$RED";"$BG$DEFAULT"m"$TIME_FULL
 fi
