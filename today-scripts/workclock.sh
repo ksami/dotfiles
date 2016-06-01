71f19336ba57ecbb1333745c604f133a80f5ab61
ksami <ksami.ihide@gmail.com>
2016-06-01 18:40:08 +0800

    H has leading zero but k doesnt

--- today-scripts/workclock.sh
+++ today-scripts/workclock.sh
@@ -1,4 +1,4 @@
-if [ $(date +'%H') -ge 9 -a $(date +'%H') -le 17 ]; then
+if [ $(date +'%k') -ge 9 -a $(date +'%k') -le 17 ]; then
     echo "\e[32m$(date +'%l:%M %p')";
 else
     echo "\e[31m$(date +'%l:%M %p')";
