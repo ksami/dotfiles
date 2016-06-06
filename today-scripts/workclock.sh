88a2a0f57b55c86fa91b186eaadd9550c0f54324
ksami <ksami.ihide@gmail.com>
2016-06-06 14:48:39 +0800

    add progress bar for workclock

--- today-scripts/workclock.sh
+++ today-scripts/workclock.sh
@@ -1,4 +1,3 @@
-FG_DEFAULT="39"
 FG="3"
 BG="4"
 DEFAULT="9"
@@ -12,7 +11,15 @@ CYAN="6"
 LGRAY="7"
 
 TIME_24H=$(date +'%k')
+TIME_QUART=$(( $(date +'%M') / 15 ))
 TIME_FULL=$(date +'%l:%M %p')
+
+for (( i=0; i<($TIME_24H-9)*4+$TIME_QUART; i++ ))
+do
+    echo -ne "\e["$FG$(($i/4))"m█"
+done
+
+echo -ne "\n"
 
 if [ $TIME_24H -ge 9 -a $TIME_24H -lt 12 ]; then
     echo "\e["$FG$GREEN";"$BG$DEFAULT"m"$TIME_FULL
