f2963b2354999c38f860bc76e3062b5debcd4db8
ksami <ksami.ihide@gmail.com>
2018-10-04 16:52:39 +0800

    Add pause if mouse button 4 held down

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,11 @@
 hs.urlevent.bind("karabiner", function(eventName, params)
+  app = hs.application.find("waterfox")
+  app:activate()
   if params["pause"] then
-    app = hs.application.find("waterfox")
-    app:activate()
+    -- hs.alert.show("pause")
     hs.eventtap.keyStroke({"alt", "shift"}, "p")
+  elseif params["next"] then
+    -- hs.alert.show("next")
+    hs.eventtap.keyStroke({"alt", "shift"}, "right")
   end
 end)
